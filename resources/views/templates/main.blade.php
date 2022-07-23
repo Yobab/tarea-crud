d01a2dc9bf8c406bff92b71e4dd84fca8557666d
Yobab <69070746+Yobab@users.noreply.github.com>
2022-07-23 11:03:47 -0600

    Update main.blade.php

--- resources/views/templates/main.blade.php
+++ resources/views/templates/main.blade.php
@@ -18,7 +18,7 @@
 <body>
     <nav class="navbar navbar-dark navbar-expand-lg bg-dark">
         <div class="container-fluid">
-            <a class="navbar-brand" href="{{ route('home') }}">Crud Customer</a>
+            <a class="navbar-brand" href="{{ route('home') }}">Crud</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup"
                 aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
